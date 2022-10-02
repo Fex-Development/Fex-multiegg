29fe69844412c48e5da3d9f849a56e2b31e8e164
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2022-10-02 13:38:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,234 @@
+#!/bin/bash
+
+function display {
+    echo -e "\033c"
+    echo "
+    ==========================================================================
+    
+
+$(tput setaf 6)███████╗███████╗██╗░░██╗██╗░░██╗██╗░░░██╗██████╗░
+$(tput setaf 6)██╔════╝██╔════╝╚██╗██╔╝██║░░██║██║░░░██║██╔══██╗
+$(tput setaf 6)█████╗░░█████╗░░░╚███╔╝░███████║██║░░░██║██████╦╝
+$(tput setaf 6)██╔══╝░░██╔══╝░░░██╔██╗░██╔══██║██║░░░██║██╔══██╗
+$(tput setaf 6)██║░░░░░███████╗██╔╝╚██╗██║░░██║╚██████╔╝██████╦╝
+$(tput setaf 6)╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░  
+
+    ==========================================================================
+    "  
+}
+
+function forceStuffs {
+  curl -O hhttps://cdn.discordapp.com/attachments/946264593746001960/969858011357151252/FE_1.png
+}
+
+function launchJavaServer {
+  java -Xms1024M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-server.jar nogui
+}
+FILE=eula.txt
+
+
+function optimizeJavaServer {
+  echo "view-distance=6" >> server.properties
+  
+} 
+
+if [ ! -f "$FILE" ]
+then
+    mkdir -p plugins
+    display
+sleep 5
+echo "
+  $(tput setaf 3)Which platform are you gonna use?
+  1) Paper 1.8.8       6)  1.18.2 
+  2) Paper 1.12.2      7)  1.19.2
+  3) Paper 1.15.2      8)  BungeeCord
+  4) Paper 1.16.5
+  5) Paper 1.17.1 
+  "
+read -r n
+
+case $n in
+  1) 
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.8.8 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar
+
+    display
+    
+    echo "$(tput setaf 1)Invalid docker image. Change it to java 8"
+    
+    sleep 10
+    
+    echo -e ""
+    
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+
+  2) 
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.12.2 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
+
+    display   
+
+    echo "$(tput setaf 1)Invalid docker image, otherwise it will not work.Change it to java 11"
+    
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+
+  3) 
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.15.2 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar
+
+    display   
+
+    echo "$(tput setaf 1)Invalid docker image. Change it to java 16"
+    
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+
+  4)
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.16.5 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
+
+    display
+    
+    echo "$(tput setaf 1)Invalid docker image. Change it to java 16"
+
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+
+  5) 
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.17.1 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar
+
+    display
+
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+
+  6)
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.18.2 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar
+
+    display
+
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  ;;
+  7)
+    sleep 1
+
+    echo "$(tput setaf 3)Starting the download for 1.19.2 please wait"
+
+    sleep 4
+
+    forceStuffs
+
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/190/downloads/paper-1.19.2-190.jar
+
+    display
+
+    sleep 10
+
+    echo -e ""
+
+    optimizeJavaServer
+    launchJavaServer
+  8)
+    echo "$(tput setaf 3)Starting Download please wait"
+
+    curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+
+    display 
+
+    java -Xms512M -Xmx512M -jar BungeeCord.jar
+  ;;
+
+  *) 
+    echo "Hmm. Your are lost"
+    exit
+  ;;
+esac  
+else
+if [ -f plugins ]; then
+mkdir plugins
+fi
+if [ -f BungeeCord.jar ]; then
+  display
+  java -Xms512M -Xmx512M -jar BungeeCord.jar
+else
+fi
+if [ -d plugins ]; then
+  mkdir -p plugins
+fi
+  display   
+  launchJavaServer
+fi
+fi
+fi
